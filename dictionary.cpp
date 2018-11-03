8ec215159ec1be1db6ba0bd0a6a51a3adf77ff8d
Owntrolf <klo74@my.bcit.ca>
2018-11-02 23:01:20 -0700

    completed implementing functions and testing

--- dictionary.cpp
+++ dictionary.cpp
@@ -5,13 +5,50 @@
 #include "dictionary.hpp"
 
 dictionary::dictionary(std::string location){
-    loc = location;
+    loc = location; //location of the text file
+    fillDict();
 }
 
 dictionary::~dictionary(){
 
 }
 
+//Fills the dictionary map with data from the provided textfile
+void dictionary::fillDict() {
+    bool newWord = true;
+    std::string word = "";
+    std::string def = "";
+
+    std::string readIn = "";
+    std::ifstream fDict(loc);
+    if(fDict.is_open())
+    {
+        while(getline(fDict, readIn))
+        {
+            if(newWord) //will be a new word if newWord is true
+            {
+                word = readIn;
+                newWord = false;
+            }
+            else //will be a definition if newWord is false
+            {
+                def = readIn;
+                dMap.insert(make_pair(word, def));
+                newWord = true;
+            }
+        }
+
+        fDict.close();
+
+    }
+    else
+    {
+        std::cout << "Unable to open file\n";
+    }
+
+}
+
+//The menu of the dictionary that keeps repeating until exited by entering 4
 void dictionary::menu() {
     int option = -1;
     while(option != 4)
@@ -34,52 +71,55 @@ void dictionary::menu() {
     }
 }
 
+//prints the entire dictionary
 void dictionary::printDict() {
-    bool newWord = true;
-    std::string word = "";
-    std::string def = "";
-
-    std::string readIn = "";
-    std::ifstream fDict(loc);
-    if(fDict.is_open())
+    for(dIterator = dMap.begin(); dIterator != dMap.end(); dIterator++)
     {
-        while(getline(fDict, readIn))
-        {
-            //std::cout << readIn << '\n';
-            if(newWord)
-            {
-                word = readIn + '\n';
-                newWord = false;
-            }
-            else
-            {
-                def = readIn + '\n';
-                dMap.insert(make_pair(word, def));
-                //std::cout << word << "||" << def << std::endl;
-                newWord = true;
-            }
-        }
-
-        fDict.close();
-
-        for(dIterator = dMap.begin(); dIterator != dMap.end(); dIterator++)
-        {
-            std::cout<< dIterator->first << " - " << dIterator->second << std::endl;
-        }
+        std::cout << dIterator->first << " - " << dIterator->second << "\n" << std::endl;
     }
-    else
-    {
-        std::cout << "Unable to open file\n";
-    }
-
 }
 
+//finds definition of a select word
 void dictionary::findDef() {
-    
+    std::string toFind;
+
+    std::cout << "Enter the word's definition you want to find" << std::endl;
+    std::cin >> toFind;
+    dIterator = dMap.find(toFind);
+    if(dIterator != dMap.end())
+    {
+        std::cout << dIterator->first << " - " << dIterator->second << "\n" << std::endl;
+    } else {
+        std::cout << "The word doesn't exist" << std::endl;
+    }
 }
 
+//Allows user to enter a new word into the map and the text file
 void dictionary::newWord() {
-    std::cout << "Enter the new word: ";
+    std::string newWord;
+    std::string def;
+    do {
+        std::cout << "Enter the new word: ";
+        std::cin >> newWord;
+        dIterator = dMap.find(newWord);
+        if(dIterator == dMap.end()) // not found in dictionary
+        {
+            std::cout << "Enter the definition for the word" << std::endl;
+            std::cin.ignore();
+            getline(std::cin, def);
+            dMap.insert(make_pair(newWord,def)); //adding into text file
+
+            std::ofstream fDict; //writing to text file
+            fDict.open(loc, std::ios_base::app);
+            fDict << newWord << "\n" << def << "\n";
+            fDict.close();
+        }
+        else
+        {
+            std::cout << "This word already exists, please enter a new word" << std::endl;
+        }
+    } while(dIterator != dMap.end());
+
 }
 
 void dictionary::exit() {
